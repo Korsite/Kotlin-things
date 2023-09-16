39c01fc9ec55f574ccf98ecc42f731b1a2aac2a3
Korsite <hsr49rsh@gmail.com>
2023-09-15 19:56:37 -0500

    adding some code to simplex method

--- src/Gandhi's projects/Simplex method.kt
+++ src/Gandhi's projects/Simplex method.kt
@@ -1,3 +1,8 @@
+import java.math.BigDecimal
+import java.math.RoundingMode
+import kotlin.math.roundToInt
+import kotlin.math.roundToLong
+
 /*
     z  V1  V2  Vn   S1   S2  Sn  Answer
 S1                              =
@@ -10,126 +15,258 @@ Z
  */
 
 fun main() {
-    val S1 = listOf(0, 2, 3, 1, 0, 0, 1500)
-    val S2 = listOf(0, 3, 2, 0, 1, 0, 1500)
-    val S3 = listOf(0, 1, 1, 0, 0, 1, 500)
-    val Z = listOf(1, -10, -12, 0, 0, 0, 600)
 
-    val indexPivotElement = Z.indexOf(checkTheBiggestNegativeNumber(Z))
-    val rows = listOf(
+    val S1 = listOf(0, 80, 92.5, 29, 38, 1, 0, 0, 0, 800).map { it.toDouble() }
+    val S2 = listOf(0, 0,   0,    1, 1,  0, 1, 0, 0, 5).map { it.toDouble() }
+    val S3 = listOf(0, 0,   0,   29, 38, 0, 0, 1, 0, 180).map { it.toDouble() }
+    val S4 = listOf(0, 25,  34,   0, 0,  0, 0, 0, 1, 80).map { it.toDouble() }
+    val Z = listOf( 1, -5, -85, -24,-28, 0, 0, 0, 0, 0).map { it.toDouble() }
+
+    var rows = listOf(
         S1,
         S2,
         S3,
+        S4,
         Z
     )
-    val pivotColumn = getPivotColumn(
-        rows,
-        indexPivotElement
-    )
+    var indexPivotElement = rows.last()
+        .indexOf(
+            checkTheBiggestNegativeNumber(rows.last())
+        )
 
-    val pivotRow = getPivotRow(
-        rows,
-        pivotColumn
-    )
 
-    println(
-        "$pivotColumn \n$pivotRow"
-    )
+    while(rows.last()[indexPivotElement] < 0) {
 
-    println(
-        convertPivotElementIntoOne(
-            pivotRow,
+        /*
+        this variable stores the index where the minus negative number in
+        my object function is, example:
+        having a list of (1, -3, -4 , -5), this stores the index 3
+        because [3] = -5
+        */
+
+        indexPivotElement = rows.last()
+            .indexOf(checkTheBiggestNegativeNumber(rows.last()))
+
+        val pivotColumn = getPivotColumn(
+            rows,
             indexPivotElement
         )
-    )
 
+        println()
+        println("pivto Colummn: $pivotColumn")
+
+        val pivotRow = getPivotRow(
+            rows,
+            pivotColumn
+        )
+
+        println()
+        println("pivto row: $pivotRow")
+        println()
+
+        rows = rows.map {
+            val newRowWherePivotElementIsOne = convertPivotElementIntoOne(
+                pivotRow,
+                pivotRow[indexPivotElement]
+            )
+
+            if (it == pivotRow)
+                newRowWherePivotElementIsOne
+            else
+                convertElementIntoZero(
+                    newRowWherePivotElementIsOne,
+                    it,
+                    indexPivotElement
+                )
+        }
+
+        rows.forEach {
+            println(it)
+        }
+    }
 
 }
 
-fun checkTheBiggestNegativeNumber(zList: List<Int>): Int {
+fun checkTheBiggestNegativeNumber(zList: List<Double>): Double {
     return if (zList.any { it < 0 }) zList.filter { it < 0 }.min()
-    else 0
+    else 0.0
 }
 
 /**
+ * Returns a list of elements of every list at index
+ * where the major negativity is found
  *
+ * major negativity is given by the checkTheBiggestNegativeNumber()
+ * used in objective function
+ */
+
+/*
+for example, given an interaction of the next table
+
+    z  V1   V2  V3   S1   S2   Answer
+S1  0   1   >3   5   1    0  =  100
+
+S2  0   3   >4   1   0    1  =  160
+
+Z   10 -3   -4  -1   0    0  =  160
+
+My pivot Column would be [3, 4], because -4 is the most minimum among the Z entries
  */
 fun getPivotColumn(
-    rows: List<List<Int>>,
-    biggestNegativeNumber: Int
-): List<Int> {
-    val list = mutableListOf<Int>()
+    rows: List<List<Double>>,
+    indexWhereBiggestNegativeNumber: Int
+): List<Double> {
+    val list = mutableListOf<Double>()
 
-    fun checkTheBiggestNegativeNumber(zList: List<Int>): Int {
-        return if (zList.any { it < 0 }) zList.filter { it < 0 }.min()
-        else 0
-    }
+    /*
+     I only need elements from the pivot column which do not belong to the Z function
+     and since Z function is the last element in the pivot Element then I can
+     create a sublist where I don't take last list
+     */
+    for (listOfRestrictions in rows.subList(0, rows.lastIndex))
+        list.add(listOfRestrictions[indexWhereBiggestNegativeNumber])
 
-    rows.forEach {
-        list.add(it[biggestNegativeNumber])
-    }
     return list
 }
 
-/** We need first the pivot column
- * Once found it, the second parameter of this function
- * is useful to divide by the results (the last entry of every list)
+/**
+ * pivot column is a must to apply this function
+ * This returns the list where the lowest number of dividing
+ * Answer / element found in the pivot column (in the same row) is found
+ *
+ * this also gives us which the pivot Element is,
+ * Where the pivot column and pivot row intersects is where I have my pivot element
  */
 
 /*
 for example, given an interaction of the next table
 
     z  V1  V2  V3   S1   S2   Answer
-S1  0   1   3   5   1    0  =  100
+S1  0   1   3   5   1    0  =  >100
 
-S2  0   3   4   1   0    1  =  160
+S2  0   3   4   1   0    1  =  >160
 
 Z   10 -3  -4  -1   0    0  =  160
 
-My pivot Column would be here [3, 4, -4], because -4 is the most minimum among the Z entries
-My row column would the lowest number dividing the answer by the number found in the pivot column
+My pivot Column = [3, 4]
+My pivot Row would be the row with the lowest number dividing the answer by the number
+found in the pivot column
 
 For S1 -> Answer / elementFoundInPivotColumn = 100 / 3 = 33.33
 For S2 -> 160 / 4 = 40
 
-We check which is the most minimum number
+We check which is the major negative number, and in this case is for S1, because 33.33 < 40
+So my pivot row would be [0, 1, 3, 5, 1, 0, 100], that list of elements
+is what this function returns
 
-kkkk
+And my pivot element is where pivot row and column intersects
+[0, 1, [3], 5, 1, 0, 100]
+       [4]
+       pivot element is 3!
  */
 
 fun getPivotRow(
-    rows: List<List<Int>>,
-    numbersWhichWillDivide: List<Int>
-): List<Int> {
-    val numbersDivided = mutableListOf<Int>()
+    rows: List<List<Double>>,
+    numbersWhichWillDivide: List<Double>
+): List<Double> {
 
-    // until, because I don't want to take Z objective function
-    for (i in 0 until rows.size - 1) {
-        numbersDivided.add(
-            rows[i].last() / numbersWhichWillDivide[i]
-        )   
+    // last because that is the result ( = x ) / divided by the element found in the pivot column
+    var lowestNumberWhenDivided = rows[0].last() / numbersWhichWillDivide[0]
+    var indexWhereLowestNumberWhenDivided = 0
+
+    numbersWhichWillDivide.forEachIndexed { index, numberToDivide ->
+        val resultDividedByTheElementFoundInPivotColumn = rows[index].last() / numberToDivide
+        println("${rows[index].last()} / $numberToDivide = $resultDividedByTheElementFoundInPivotColumn")
+
+
+        if (resultDividedByTheElementFoundInPivotColumn < lowestNumberWhenDivided){
+            lowestNumberWhenDivided = resultDividedByTheElementFoundInPivotColumn
+            indexWhereLowestNumberWhenDivided = index
+        }
     }
-
-    return rows[
-        numbersDivided.indexOf(numbersDivided.min())
-    ]
+    return rows[indexWhereLowestNumberWhenDivided]
 }
 
+/**
+ * Pivot Row is a must to make this function works,
+ * returns the same Pivot Row but multiplied by 1/x,
+ * where x is the pivotElement.
+ * Check comments in getPivotRow() function to know about pivotElement
+ */
 /*
  this function makes this
     Example:
         [10, 5, 8, 10, 12] let's say pivot Element is [2] = 8
         in order to convert the number 8 into one, it has to be multiplied by 1/8
         but the other elements also have to be multiplied by 1/8
-        so my list would be like this -> [10/8, 2/8, 1, 10/8, 12/8]
-
-
+        so my list would end up like this -> [10/8, 2/8, 1, 10/8, 12/8]
  */
 fun convertPivotElementIntoOne(
-    pivotRow: List<Int>,
-    pivotElement: Int
+    pivotRow: List<Double>,
+    pivotElement: Double
 ): List<Double> {
+    val formatTheBigNumbers: (Double) -> Double = { numberToRound ->
+        BigDecimal(numberToRound).setScale(4, RoundingMode.DOWN).toDouble()
+    }
     return pivotRow.map {
-        (1 / pivotElement.toDouble()) * it
+        formatTheBigNumbers(1 / pivotElement * it)
+    }
+}
+
+/**
+ * This is applied to every list which is NOT the pivot Row
+ * Returns the same list but turning the element at index where pivot element is 0
+ */
+
+/*
+for example, given an interaction of the next table
+where pivotElement is converted into one in pivotRow
+
+    z    V1   V2   V3    S1    S2   Answer
+S1  0   1/3   1   5/3   1/3    0  =  100/3
+
+S2  0     3   >4     1     0    1  =  160
+
+Z   10   -3  >-4    -1     0    0  =  160
+
+S2 at index 2 = 4
+We have to convert that 4 in 0, so we multiply S1 * -4, and then
+we add to S2, so the formula would be S1 * -4 + S2, I am lazy to do the
+operation, but I know that if 1 * -4 + 4 = 0, then what I want is right so
+this function would return [?, ?, 0, ?, ?, ?, ?]
+
+so in order to do Z at index 2 = 0
+we have to S1 * 4 + Z
+ */
+fun convertElementIntoZero(
+    listWherePivotElementIsConvertedIntoOne: List<Double>,
+    listToConvert0AtIndexOfPivotElement: List<Double>,
+    indexWherePivotElement: Int
+): List<Double> {
+    val numberToMultiplyTheListWherePivotElementConvertedIntoOne =
+        listToConvert0AtIndexOfPivotElement[indexWherePivotElement] * -1
+
+    val formatTheBigNumbers: (Double) -> Double = { numberToRound ->
+        BigDecimal(numberToRound).setScale(6, RoundingMode.DOWN).toDouble()
+    }
+
+    return listWherePivotElementIsConvertedIntoOne.mapIndexed { index, i ->
+
+        // aqui me quede, tengo que encontrar la manera de redondear los numeros
+        //
+        println("$i * $numberToMultiplyTheListWherePivotElementConvertedIntoOne + ${listToConvert0AtIndexOfPivotElement[index]} = " +
+                "${i* numberToMultiplyTheListWherePivotElementConvertedIntoOne + listToConvert0AtIndexOfPivotElement[index]}"
+        )
+        println("$i * $numberToMultiplyTheListWherePivotElementConvertedIntoOne + ${listToConvert0AtIndexOfPivotElement[index]} = (rounded) " +
+                "${
+                    formatTheBigNumbers(i* numberToMultiplyTheListWherePivotElementConvertedIntoOne + listToConvert0AtIndexOfPivotElement[index])
+                }"
+        )
+        println()
+
+
+
+        formatTheBigNumbers(i * numberToMultiplyTheListWherePivotElementConvertedIntoOne +
+                listToConvert0AtIndexOfPivotElement[index])
     }
 }
