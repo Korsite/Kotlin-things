be3f9221cbb507844ceef60a6e25f2d2eb93efe9
Korsite <hsr49rsh@gmail.com>
2024-01-31 07:54:26 -0600

    nothing important

--- src/heidyAndItzel.kt
+++
@@ -1,8 +0,0 @@
-/*** Inicialicen la funcion principal, lean 2 valores, e impriman lo siguiente:
- * Suma
- * Resta
- * Division
- * Y multiplicacion
- *
- * Y al final, el resultado de dichas operaciones, sea TODO sumado
- */
